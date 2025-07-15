9bbc74d64f032ef54499e68c40d4b04142439642
ShawayL <1610242711@vip.henu.edu.cn>
2025-07-15 09:02:06 +0800

    添加测试模块及相关 CMake 配置

---
+++ test_cpp_lab/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cpp_lab/sample_library.hpp>
+
+int main() {
+    std::cout << "Testing cpp_lab::sample_library\n";
+
+    std::cout << "factorial(5)="
+              << factorial(5) << std::endl;
+
+    return 0;
+}
